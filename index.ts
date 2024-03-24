46bbb027440ae5e91b25d6a226fd125dbda06992
mubi715 <rafiquemubasher56@gmail.com>
2024-03-24 14:43:33 +0500

    some changes in message

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter secound number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your secound number", type: "number", name: "secondNumber" },
   { message: "select one of operator to selection",type:"list",name:"operator",choices:["addition","subtraction","multiplicatio","division"] },
   
 ]);
